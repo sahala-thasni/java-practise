f429966f6f587dcb36343af0245bdfa783ca3c5b
sahala thasni <sahalathasni2003@gmail.com>
2026-01-16 10:27:16 +0530

    Updated file content

--- pattern.java
+++ pattern.java
@@ -1,14 +1,25 @@
 public class pattern{
     public static void main(String args[]){
-        int n=4;
-        for(int i=0;i<=n;i++){
-            for(int j=0;j<=n;j++){
-                if(j<=(n-Math.abs(i))){
+        int n=7;
+        int m=1;
+        boolean k=false;
+        for(int i=1;i<=n*2;i++){
+            for(int j=1;j<=m;j++){
+                if((i+j)%2 ==0){
                     System.out.print(" *");
                 }
                 else{
                     System.out.print(" ");
                 }
+            }
+            if(m==n){
+                k=true;
+            }   
+            if(k==false){
+                    m++;
+            }
+            else{
+                    m--;
             }
             System.out.println();
         }
